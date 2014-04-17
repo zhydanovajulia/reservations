5875c88aaf7a8406bc1277de5d1e1038a35f134e
Julia Zhydanova <jz@anahoret.com>
2014-04-17 23:40:55 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reservations::Application.config.secret_token = 'e536abd4109ab6d25d3713c0919394a84e596902166851fe22cc4487c4c6503858a49e09ceb30929b387321184d4c906bac25da599ca2f0a2cfb80927555cbd2'
