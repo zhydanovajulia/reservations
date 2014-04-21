6cfebc113f0f5bbcdcab3c2f33b9c2c13adc9449
Julia Zhydanova <jz@anahoret.com>
2014-04-21 17:44:31 +0300

    Using scopes looks more pretty

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,24 +4,30 @@ class Reservation < ActiveRecord::Base
   validate :time_in_the_past
   validate :time_range
 
-  scope :table, -> (table_number){ where(table_number: table_number) }
+  scope :table, ->(table_number) { where(table_number: table_number) }
+  scope :not_me, ->(id) { where.not(id: id) }
+  scope :intersection, ->(start_time, end_time) { where("(start_time <= :start_time AND end_time > :start_time)
+                                                  OR (start_time < :end_time AND end_time >= :end_time)
+                                                  OR(start_time >= :start_time AND end_time <= :end_time)",
+                                                  start_time: start_time, end_time: end_time) }
 
   private
 
   def time_in_the_past
-    self.errors.add(:start_time, "can't be in the past") if self.start_time && self.start_time < DateTime.current
+    self.errors.add(:start_time, "can't be in the past") if self.start_time && (self.start_time < DateTime.current)
   end
 
   def time_range
-    self.errors.add(:start_time, "can't go after the end time") if self.start_time && self.start_time > self.end_time && self.end_time
+    self.errors.add(:start_time, "can't go after the end time") if time_present && (self.start_time > self.end_time)
   end
 
   def time_intersection
-    if self.class.table(self.table_number).where.not(id: self.id).where("(start_time <= :start_time AND end_time > :start_time)
-                                                  OR (start_time < :end_time AND end_time >= :end_time)
-                                                  OR(start_time >= :start_time AND end_time <= :end_time)",
-      start_time: self.start_time, end_time: self.end_time).any?
+    if self.class.table(self.table_number).not_me(self.id).intersection(self.start_time, self.end_time).any?
       self.errors.add(:time, 'intersection')
     end
+  end
+
+  def time_present
+    self.start_time && self.end_time
   end
 end
